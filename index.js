6de9041c0ff99402463744883777a43b6d1b08f9
Maksimalniy <xborcci@gmail.com>
2021-11-05 13:46:19 +0300

    1.5

---
+++ index.js
@@ -0,0 +1,12 @@
+const slider = document.querySelector('.swiper-container')
+
+const swiper = new Swiper(slider, {
+    spaceBetween: 16,
+    slidesPerView: "auto",
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+    },
+})
+console.log (slider)
